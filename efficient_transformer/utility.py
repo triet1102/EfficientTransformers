86b595d2e0177aae1a444c15b9d94ae22ab6fb91
triet1102 <t.triet.1102@gmail.com>
2024-01-30 15:23:31 +0100

    compare different types of training

--- efficient_transformer/utility.py
+++ efficient_transformer/utility.py
@@ -1,13 +1,12 @@
 import pickle
-import os
+
+import matplotlib.pyplot as plt
+import pandas as pd
 
 
 def save_benchmark_result(res: dict, file_path: str) -> None:
-    if os.path.exists(file_path):
-        os.remove(file_path)
-    else:
-        with open(file_path, "wb") as handle:
-            pickle.dump(res, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    with open(file_path, "wb") as handle:
+        pickle.dump(res, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
 
 def load_benchmark_result(file_path: str) -> dict:
@@ -15,3 +14,42 @@ def load_benchmark_result(file_path: str) -> dict:
         res = pickle.load(handle)
 
     return res
+
+
+def plot_metrics(perf_metrics, current_optim_type, save_path):
+    df = pd.DataFrame.from_dict(perf_metrics, orient="index")
+
+    for idx in df.index:
+        df_opt = df.loc[idx]
+        # Add a dashed circle around the current optimization type
+        if idx == current_optim_type:
+            plt.scatter(
+                df_opt["time_avg_ms"],
+                df_opt["accuracy"] * 100,
+                alpha=0.5,
+                s=df_opt["size_mb"],
+                label=idx,
+                marker="$\u25CC$",
+            )
+        else:
+            plt.scatter(
+                df_opt["time_avg_ms"],
+                df_opt["accuracy"] * 100,
+                s=df_opt["size_mb"],
+                label=idx,
+                alpha=0.5,
+            )
+
+    legend = plt.legend(bbox_to_anchor=(1, 1))
+    for handle in legend.legendHandles:
+        handle.set_sizes([20])
+
+    plt.ylim(80, 90)
+    # Use the slowest model to define the x-axis range
+    xlim = int(perf_metrics["BERT baseline"]["time_avg_ms"] + 3)
+    plt.xlim(1, xlim)
+    plt.ylabel("Accuracy (%)")
+    plt.xlabel("Average latency (ms)")
+    if save_path:
+        plt.savefig(save_path)
+    plt.show()
